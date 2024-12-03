9464dc5766b7b4e4924d750d735e1ac78431ee23
Fred <fred_p1@hotmail.co.uk>
2024-12-03 22:27:14 +0000

    There's probably a regex way of doing this part

--- 3/main.go
+++ 3/main.go
@@ -6,10 +6,25 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func mul(a int, b int) int {
 	return a * b
+}
+
+func sumMul(matches [][]string) int {
+	mulSum := 0
+	for _, match := range matches {
+		a, _ := strconv.Atoi(match[1])
+		b, _ := strconv.Atoi(match[2])
+
+		result := mul(a, b)
+
+		fmt.Printf("mul(%d, %d) = %d\n", a, b, result)
+		mulSum += result
+	}
+	return mulSum
 }
 
 func main() {
@@ -26,21 +41,33 @@ func main() {
 	for scanner.Scan() {
 		inputString += scanner.Text() + " "
 	}
+	var result []string
 
-	mulSum := 0
+	parts := strings.Split(inputString, "don't()")
+
+	//Couldn't figure out a regex for this :(
+	for i, part := range parts {
+		if i == 0 {
+			result = append(result, part)
+		} else {
+			if idx := strings.Index(part, "do()"); idx != -1 {
+				result = append(result, part[idx+len("do()"):])
+			}
+		}
+	}
+
+	onlyDos := strings.Join(result, "")
+
 	validRegex := `mul\(([0-9]{1,3}),([0-9]{1,3})\)`
 	re := regexp.MustCompile(validRegex)
 
-	matches := re.FindAllStringSubmatch(inputString, -1)
+	// doRegex := regexp.MustCompile(`do\(\)([^d]+(?:d(?!on't\().)*)`)
 
-	for _, match := range matches {
-		a, _ := strconv.Atoi(match[1])
-		b, _ := strconv.Atoi(match[2])
+	// splitString := doRegex.Split(inputString, -1)
 
-		result := mul(a, b)
+	// fmt.Println(splitString)
 
-		fmt.Printf("mul(%d, %d) = %d\n", a, b, result)
-		mulSum += result
-	}
-	fmt.Println(mulSum)
+	matches := re.FindAllStringSubmatch(onlyDos, -1)
+
+	fmt.Println(sumMul(matches))
 }
